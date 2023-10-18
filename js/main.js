3c50f85992fd0db1ef02ba627d861bb89b22b1c5
Felipe Lentini <99766298+bajo3@users.noreply.github.com>
2023-10-18 08:09:19 -0300

    hamburger nav

--- js/main.js
+++ js/main.js
@@ -6,6 +6,34 @@ const screenHeight = window.innerHeight;
 const speedX = 2; // Velocidad horizontal
 const speedY = 2; // Velocidad vertical
 
+function toggleMenu() {
+    var menu = document.getElementById('navMenu');
+    if (menu.style.display === 'none') {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+}
+
+// Comprueba el tamaño de la pantalla al cargar la página
+window.onload = function() {
+    if (window.innerWidth <= 768) {
+        document.getElementById('hamburger').style.display = 'block';
+    } else {
+        document.getElementById('navMenu').style.display = 'flex';
+    }
+};
+
+// Comprueba el tamaño de la pantalla al hacer zoom
+window.onresize = function() {
+    if (window.innerWidth <= 768) {
+        document.getElementById('hamburger').style.display = 'block';
+    } else {
+        document.getElementById('navMenu').style.display = 'block';
+        document.getElementById('hamburger').style.display = 'none';
+    }
+};
+
 let positionX = 0;
 let positionY = 0;
 let directionX = 1;
