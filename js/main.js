f53b0bb46fca9cfe9129454bd1ea9450bdcac0d3
Felipe Lentini <99766298+bajo3@users.noreply.github.com>
2023-10-19 19:22:40 -0300

    cambiamos api de crypto

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ window.onload = function () {
     loader.style.opacity = '0';
     setTimeout(function () {
         loader.style.display = 'none';
-    }, 500); // Después de 0.5 segundos, oculta el loader
+    }, 500); // Después de 0.5 segundos, oculta el loader 
 
     // Muestra el contenido principal
     var content = document.getElementsByTagName('main')[0];
@@ -33,59 +33,52 @@ window.onload = function () {
         checkScreenSize();
     };
 
-    // Almacena los precios anteriores
-    const previousPrices = {
-        "bitcoin": 0,
-        "ethereum": 0,
-        "binancecoin": 0
-    };
+   
+}
 
-    // Función para obtener el precio de una criptomoneda
-    function getCoinPrice(coinSymbol, displayElement) {
-        const url = `https://api.coingecko.com/api/v3/simple/price?ids=${coinSymbol}&vs_currencies=usd`;
-
-        $.ajax({
-            url: url,
-            method: "GET",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            success: function (data) {
-                const currentPrice = data[coinSymbol].usd;
-                const priceElement = $(displayElement);
-
-                // Compara con el precio anterior
-                if (previousPrices[coinSymbol] < currentPrice) {
-                    priceElement.css("color", "green"); // Precio subió (verde)
-                } else if (previousPrices[coinSymbol] > currentPrice) {
-                    priceElement.css("color", "red"); // Precio bajó (rojo)
-                } else {
-                    priceElement.css("color", "black"); // Precio sin cambios
-                }
-
-                // Almacena el precio actual como precio anterior
-                previousPrices[coinSymbol] = currentPrice;
-            },
-            error: function () {
-                $(displayElement).text("Error al obtener el precio.");
-            }
-        });
-    }
-
-    $(document).ready(function () {
-        // Actualizar precios en tiempo real al cargar la página
-        getCoinPrice("bitcoin", "#btc-price");
-        getCoinPrice("ethereum", "#eth-price");
-        getCoinPrice("binancecoin", "#bnb-price");
-
-        // Actualizar precios cada 60 segundos 
-        setInterval(function () {
-            getCoinPrice("bitcoin", "#btc-price");
-            getCoinPrice("ethereum", "#eth-price");
-            getCoinPrice("binancecoin", "#bnb-price");
-        }, 60000);
-    });
-
+// Función para obtener el precio de una criptomoneda en dólares (USD)
+const previousPrices = {
+    "bitcoin": 0,
+    "ethereum": 0,
+    "binancecoin": 0
 };
 
+function getCoinPrice(coinSymbol, displayElement) {
+    const url = `https://api.coingecko.com/api/v3/simple/price?ids=${coinSymbol}&vs_currencies=usd`;
 
+    $.ajax({
+        url: url,
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        success: function(data) {
+            const currentPrice = data[coinSymbol].usd;
+            const priceElement = $(displayElement);
+
+            if (previousPrices[coinSymbol] < currentPrice) {
+                priceElement.css("color", "green"); // Precio subió, color verde
+            } else if (previousPrices[coinSymbol] > currentPrice) {
+                priceElement.css("color", "red"); // Precio bajó, color rojo
+            }
+
+            priceElement.text(`$${currentPrice} USD`);
+            previousPrices[coinSymbol] = currentPrice;
+        },
+        error: function() {
+            $(displayElement).text("Error al obtener el precio.");
+        }
+    });
+}
+
+// Cuando la página se carga, obtener y mostrar los precios en dólares (USD)
+$(document).ready(function() {
+    // Llama a la función para obtener el precio de Bitcoin en dólares (USD)
+    getCoinPrice("bitcoin", "#btc-price");
+
+    // Llama a la función para obtener el precio de Ethereum en dólares (USD)
+    getCoinPrice("ethereum", "#eth-price");
+
+    // Llama a la función para obtener el precio de Binance Coin en dólares (USD)
+    getCoinPrice("binancecoin", "#bnb-price");
+});
